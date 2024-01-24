fc30801f20e866323adc70961ab776c6d5d39796
LeeByeongMuk <mansogod@gmail.com>
2024-01-25 00:26:57 +0900

    feature: add common layout

---
+++ src/app/notice/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '@/app/components/Layout';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Layout>{children}</Layout>;
+}
