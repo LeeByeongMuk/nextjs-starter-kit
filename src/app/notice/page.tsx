38b1fd0dd8a7b3496ed5f40427f3100fe9d6a45e
LeeByeongMuk <mansogod@gmail.com>
2024-01-25 01:03:49 +0900

    feature: add notice page

---
+++ src/app/notice/page.tsx
@@ -0,0 +1,233 @@
+export default function Notice() {
+  return (
+    <section className="pb-8 pt-8">
+      <div className="flex justify-between">
+        <div>
+          <div className="sm:hidden">
+            <label htmlFor="Tab" className="sr-only">
+              Tab
+            </label>
+
+            <select id="Tab" className="w-full rounded-md border-gray-200">
+              <option>Settings</option>
+              <option>Messages</option>
+              <option>Archive</option>
+              <option selected={true}>Notifications</option>
+            </select>
+          </div>
+
+          <div className="hidden sm:block">
+            <nav className="flex gap-6" aria-label="Tabs">
+              <a
+                href="#"
+                className="shrink-0 rounded-lg p-2 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Settings
+              </a>
+
+              <a
+                href="#"
+                className="shrink-0 rounded-lg p-2 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Messages
+              </a>
+
+              <a
+                href="#"
+                className="shrink-0 rounded-lg p-2 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+              >
+                Archive
+              </a>
+
+              <a
+                href="#"
+                className="shrink-0 rounded-lg bg-sky-100 p-2 text-sm font-medium text-sky-600"
+                aria-current="page"
+              >
+                Notifications
+              </a>
+            </nav>
+          </div>
+        </div>
+
+        <div className="relative">
+          <label htmlFor="Search" className="sr-only">
+            {' '}
+            Search{' '}
+          </label>
+
+          <input
+            type="text"
+            id="Search"
+            placeholder="Search for..."
+            className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+          />
+
+          <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+            <button type="button" className="text-gray-600 hover:text-gray-700">
+              <span className="sr-only">Search</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </button>
+          </span>
+        </div>
+      </div>
+
+      <div className="mt-3 overflow-x-auto">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Name
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Date of Birth
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Role
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Salary
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y divide-gray-200">
+            <tr className="odd:bg-gray-50">
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                John Doe
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                24/05/1995
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                Web Developer
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                $120,000
+              </td>
+            </tr>
+
+            <tr className="odd:bg-gray-50">
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Jane Doe
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                04/11/1980
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                Web Designer
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                $100,000
+              </td>
+            </tr>
+
+            <tr className="odd:bg-gray-50">
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Gary Barlow
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                24/05/1995
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                Singer
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                $20,000
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <ol className="mt-3 flex justify-center gap-1 text-xs font-medium">
+        <li>
+          <a
+            href="#"
+            className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900"
+          >
+            <span className="sr-only">Prev Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#"
+            className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+          >
+            1
+          </a>
+        </li>
+
+        <li className="block h-8 w-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white">
+          2
+        </li>
+
+        <li>
+          <a
+            href="#"
+            className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+          >
+            3
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#"
+            className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+          >
+            4
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#"
+            className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900"
+          >
+            <span className="sr-only">Next Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </li>
+      </ol>
+    </section>
+  );
+}
