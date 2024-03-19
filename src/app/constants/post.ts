56eaeb7d2d7d2e6a28ee45faf9e0545a39e32ffe
LeeByeongMuk <mansogod@gmail.com>
2024-03-20 02:38:37 +0900

    fix: post update page
    #18

---
+++ src/app/constants/post.ts
@@ -0,0 +1,7 @@
+const TYPE_OPTIONS = [
+  { value: '', label: 'Please select' },
+  { value: 'notice', label: 'Notice' },
+  { value: 'faq', label: 'Faq' },
+];
+
+export { TYPE_OPTIONS };
--- src/app/post/[id]/update/page.tsx
+++ src/app/post/[id]/update/page.tsx
@@ -2,9 +2,14 @@
 
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import useSWR from 'swr';
 
+import ButtonBox from '@/app/components/Post/Form/ButtonBox';
+import ContentsEditor from '@/app/components/Post/Form/ContentsEditor';
+import IsOpenCheckbox from '@/app/components/Post/Form/IsOpenCheckbox';
+import TitleInput from '@/app/components/Post/Form/TitleInput';
+import TypeSelect from '@/app/components/Post/Form/TypeSelect';
 import { fetchApi } from '@/app/utils/api';
 
 interface PostFormInput {
@@ -18,8 +23,13 @@ export default function PostUpdate() {
   const params = useParams<{ id: string }>();
   const router = useRouter();
 
+  const methods = useForm<PostFormInput>();
+  const { handleSubmit, setValue } = methods;
+
   const {
     data: { data: post },
+    isLoading,
+    error,
   } = useSWR(
     `/api/posts/${params.id}`,
     async url => {
@@ -36,10 +46,20 @@ export default function PostUpdate() {
           is_open: null,
         },
       },
+      revalidateOnFocus: false,
+      shouldRetryOnError: false,
+      onSuccess: ({ data: post }) => {
+        setValue('title', post.title);
+        setValue('contents', post.contents);
+        setValue('type', post.type);
+        setValue('isOpen', post.is_open);
+      },
+      onError: () => {
+        alert('Failed to fetch post');
+        router.push('/post');
+      }
     }
   );
-
-  const { register, handleSubmit } = useForm<PostFormInput>();
 
   const onSubmit: SubmitHandler<PostFormInput> = async data => {
     try {
@@ -60,101 +80,23 @@ export default function PostUpdate() {
     }
   };
 
-  const redirectBack = () => {
-    router.back();
-  };
+  if (isLoading || error) return <div>Loading...</div>;
 
   return (
-    <section className="mt-5 flex min-h-full flex-col justify-center border-2 border-teal-600 px-6 py-12 lg:px-8">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label
-            htmlFor="title"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Title
-          </label>
+    <FormProvider {...methods}>
+      <section className="mt-5 flex min-h-full flex-col justify-center border-2 border-teal-600 px-6 py-12 lg:px-8">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TitleInput defaultValue={post.title} />
 
-          <input
-            type="text"
-            id="title"
-            className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-            placeholder="Post title..."
-            defaultValue={post.title}
-            {...register('title', { required: true })}
-          />
-        </div>
+          <TypeSelect selectedValue={post.type}  />
 
-        <div className="mt-5">
-          <label
-            htmlFor="type"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Type
-          </label>
+          <ContentsEditor defaultValue={post.contents} />
 
-          <select
-            id="type"
-            className="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
-            defaultValue={post.type}
-            {...register('type')}
-          >
-            <option value="">Please select</option>
-            <option value="notice">Notice</option>
-            <option value="faq">Faq</option>
-          </select>
-        </div>
+          <IsOpenCheckbox checked={post.is_open} />
 
-        <div className="mt-5">
-          <label
-            htmlFor="contents"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Contents
-          </label>
-
-          <textarea
-            id="contents"
-            className="mt-2 w-full rounded-lg border-gray-200 align-top shadow-sm sm:text-sm"
-            placeholder="Enter any additional order notes..."
-            defaultValue={post.contents}
-            {...register('contents', { required: true })}
-          />
-        </div>
-
-        <div className="mt-5">
-          <label
-            htmlFor="isOpen"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Is Open
-          </label>
-
-          <input
-            type="checkbox"
-            id="isOpen"
-            className="mt-1 rounded-md border-gray-200 shadow-sm sm:text-sm"
-            {...register('isOpen')}
-          />
-        </div>
-
-        <div className="mt-5 flex justify-center gap-2.5">
-          <button
-            type="button"
-            className="inline-block rounded border border-teal-600 bg-teal-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-teal-600 focus:outline-none focus:ring active:text-teal-500"
-            onClick={redirectBack}
-          >
-            back
-          </button>
-
-          <button
-            type="submit"
-            className="inline-block rounded border border-teal-600 px-12 py-3 text-sm font-medium text-teal-600 hover:bg-teal-600 hover:text-white focus:outline-none focus:ring active:bg-teal-500"
-          >
-            submit
-          </button>
-        </div>
-      </form>
-    </section>
+          <ButtonBox />
+        </form>
+      </section>
+    </FormProvider>
   );
 }
