ed9466dfe520e6cb004a6faf3d5520b86f5810c9
LeeByeongMuk <mansogod@gmail.com>
2024-03-20 02:38:16 +0900

    fix: post create page
    #18

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -2,8 +2,13 @@
 
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
+import ButtonBox from '@/app/components/Post/Form/ButtonBox';
+import ContentsEditor from '@/app/components/Post/Form/ContentsEditor';
+import IsOpenCheckbox from '@/app/components/Post/Form/IsOpenCheckbox';
+import TitleInput from '@/app/components/Post/Form/TitleInput';
+import TypeSelect from '@/app/components/Post/Form/TypeSelect';
 import { fetchApi } from '@/app/utils/api';
 
 interface PostFormInput {
@@ -15,7 +20,8 @@ interface PostFormInput {
 
 export default function PostCreate() {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<PostFormInput>();
+  const methods = useForm<PostFormInput>();
+  const { handleSubmit } = methods;
 
   const onSubmit: SubmitHandler<PostFormInput> = async data => {
     try {
@@ -36,98 +42,21 @@ export default function PostCreate() {
     }
   };
 
-  const redirectBack = () => {
-    router.back();
-  };
-
   return (
-    <section className="mt-5 flex min-h-full flex-col justify-center border-2 border-teal-600 px-6 py-12 lg:px-8">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label
-            htmlFor="title"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Title
-          </label>
+    <FormProvider {...methods}>
+      <section className="mt-5 flex min-h-full flex-col justify-center border-2 border-teal-600 px-6 py-12 lg:px-8">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TitleInput />
 
-          <input
-            type="text"
-            id="title"
-            className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-            placeholder="Post title..."
-            {...register('title', { required: true })}
-          />
-        </div>
+          <TypeSelect />
 
-        <div className="mt-5">
-          <label
-            htmlFor="type"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Type
-          </label>
+          <ContentsEditor />
 
-          <select
-            id="type"
-            className="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
-            {...register('type')}
-          >
-            <option value="">Please select</option>
-            <option value="notice">Notice</option>
-            <option value="faq">Faq</option>
-          </select>
-        </div>
+          <IsOpenCheckbox />
 
-        <div className="mt-5">
-          <label
-            htmlFor="contents"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Contents
-          </label>
-
-          <textarea
-            id="contents"
-            className="mt-2 w-full rounded-lg border-gray-200 align-top shadow-sm sm:text-sm"
-            placeholder="Enter any additional order notes..."
-            {...register('contents', { required: true })}
-          />
-        </div>
-
-        <div className="mt-5">
-          <label
-            htmlFor="isOpen"
-            className="block text-xs font-medium text-gray-700"
-          >
-            Is Open
-          </label>
-
-          <input
-            type="checkbox"
-            id="isOpen"
-            className="mt-1 rounded-md border-gray-200 shadow-sm sm:text-sm"
-            {...register('isOpen')}
-          />
-        </div>
-
-        <div className="mt-5 flex justify-center gap-2.5">
-          <button
-            type="button"
-            className="inline-block rounded border border-teal-600 bg-teal-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-teal-600 focus:outline-none focus:ring active:text-teal-500"
-            onClick={redirectBack}
-          >
-            back
-          </button>
-
-          <button
-            type="submit"
-            className="inline-block rounded border border-teal-600 px-12 py-3 text-sm font-medium text-teal-600 hover:bg-teal-600 hover:text-white focus:outline-none focus:ring active:bg-teal-500"
-          >
-            submit
-          </button>
-        </div>
-      </form>
-    </section>
+          <ButtonBox />
+        </form>
+      </section>
+    </FormProvider>
   );
 }
