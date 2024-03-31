e0c6b33f7116a9fe57b393fbe6c690d8c6d0c2a8
LeeByeongMuk <mansogod@gmail.com>
2024-03-31 17:58:26 +0900

    feature: add post list skeleton ui
    #5

--- src/app/components/Post/ListTable.tsx
+++ src/app/components/Post/ListTable.tsx
@@ -1,30 +1,94 @@
 import Link from 'next/link';
+import React from 'react';
 
 interface Props {
-  posts: { id: number; title: string; type: string }[];
+  posts: {
+    id: number;
+    title: string;
+    type: string;
+    hit: number;
+    created_at: string;
+    user: {
+      id: number;
+      name: string;
+    };
+  }[];
+  isLoading: boolean;
 }
 
-export default function ListTable({ posts }: Props) {
+const DUMMY_DATA = Array.from({ length: 10 }, (_, i) => ({
+  id: i,
+  title: `Title ${i}`,
+  type: 'Type',
+}));
+
+export default function ListTable({ posts, isLoading }: Props) {
+  const getFormattedDate = (date: string) => {
+    return new Intl.DateTimeFormat('ko-KR').format(new Date(date));
+  }
+
+  const renderPostList = () => {
+    if (isLoading) {
+      return DUMMY_DATA.map(post => (
+        <tr key={post.id} className="odd:bg-gray-50">
+          <td className="px-4 py-3.5 w-8/12">
+            <div className="w-full h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          </td>
+          <td className="px-4 py-3.5">
+            <div className="w-full h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          </td>
+          <td className="px-4 py-3.5">
+            <div className="w-full h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          </td>
+          <td className="px-4 py-3.5">
+            <div className="w-full h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          </td>
+        </tr>
+      ));
+    }
+
+    return posts.map(post => (
+      <tr key={post.id} className="odd:bg-gray-50">
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 w-8/12">
+          <Link href={`/post/${post.id}`}>{post.title}</Link>
+        </td>
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+          <Link href={`/post/${post.id}`}>{post.hit}</Link>
+        </td>
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+          <Link href={`/post/${post.id}`}>{post.user.name}</Link>
+        </td>
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+          <Link href={`/post/${post.id}`}>{getFormattedDate(post.created_at)  }</Link>
+        </td>
+      </tr>
+    ));
+  };
+
   return (
     <div className="mt-3 overflow-x-auto">
       <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
         <thead className="ltr:text-left">
           <tr>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 w-8/12">
+              Title
+            </th>
+
             <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              Name
+              Hit
+            </th>
+
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Writer
+            </th>
+
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Time
             </th>
           </tr>
         </thead>
 
-        <tbody className="divide-y divide-gray-200">
-          {posts.map(post => (
-            <tr key={post.id} className="odd:bg-gray-50">
-              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                <Link href={`/post/${post.id}`}>{post.title}</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody className="divide-y divide-gray-200">{renderPostList()}</tbody>
       </table>
     </div>
   );
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -7,7 +7,6 @@ import { fetchPosts } from '@/app/api/post';
 import ListFilter from '@/app/components/Post/ListFilter';
 import ListTable from '@/app/components/Post/ListTable';
 import Pagination from '@/app/components/Post/Pagination';
-import Spinner from '@/app/components/Spinner';
 
 export default function PostList() {
   const [page, setPage] = useState(1);
@@ -16,7 +15,7 @@ export default function PostList() {
     data: { data: posts, meta },
     isLoading,
     isError,
-    isFetching
+    isFetching,
   } = useQuery({
     queryKey: ['posts', { page }],
     queryFn: () => fetchPosts({ page }),
@@ -30,13 +29,11 @@ export default function PostList() {
     },
   });
 
-  if (isFetching|| isLoading || isError) return <Spinner />;
-
   return (
     <section className="pb-8 pt-8">
       <ListFilter />
 
-      <ListTable posts={posts} />
+      <ListTable posts={posts} isLoading={isFetching || isLoading || isError} />
 
       <Pagination meta={meta} setPage={setPage} />
     </section>
