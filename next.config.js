48bb98bc4d05efba5ca0af42d3baa731db8ad4f2
LeeByeongMuk <mansogod@gmail.com>
2024-04-02 01:40:48 +0900

    fix: add next.config.js main page redirect
    #5

--- next.config.js
+++ next.config.js
@@ -15,6 +15,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/post',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
