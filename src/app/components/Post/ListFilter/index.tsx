613ca836c9ac1f2398af65fa07bc5ccbdc8fafe6
LeeByeongMuk <mansogod@gmail.com>
2024-04-03 00:19:34 +0900

    feature: add post create button
    #5

--- src/app/components/Post/ListFilter/index.tsx
+++ src/app/components/Post/ListFilter/index.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
@@ -11,6 +13,7 @@ interface Props {
 }
 
 export default function ListFilter({ setSearchFilters }: Props) {
+  const { status } = useSession();
   const searchParams = useSearchParams();
   const methods = useForm<PostListSearch>({
     defaultValues: {
@@ -42,7 +45,18 @@ export default function ListFilter({ setSearchFilters }: Props) {
         <div className="flex justify-between">
           <TypeSelect onChange={updateTypeTrigger} />
 
-          <SearchInput />
+          <div className="flex">
+            {status === 'authenticated' && (
+              <Link
+                href="/post/create"
+                className="mr-2 flex items-center rounded border border-teal-600 bg-teal-600 px-4 text-sm font-medium text-white hover:bg-transparent hover:text-teal-600 focus:outline-none focus:ring active:text-teal-500"
+                passHref={false}
+              >
+                Post Create
+              </Link>
+            )}
+            <SearchInput />
+          </div>
         </div>
       </form>
     </FormProvider>
