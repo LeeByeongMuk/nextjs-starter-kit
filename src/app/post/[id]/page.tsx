1856c43602a448950393c2bdb11b963770c5539f
LeeByeongMuk <mansogod@gmail.com>
2024-04-02 22:09:54 +0900

    fix: add post detail page back button
    #5

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -44,11 +44,25 @@ export default function PostDetail() {
     }
   }, [isError, router]);
 
+  const redirectBack = () => {
+    router.back();
+  };
+
   return (
     <section className="pb-8 pt-8">
       {/*<ListFilter />*/}
 
       <Detail post={post} isLoading={isFetching || isLoading || isError} />
+
+      <div className="mt-5 flex justify-center gap-2.5">
+        <button
+          type="button"
+          className="inline-block rounded border border-teal-600 bg-teal-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-teal-600 focus:outline-none focus:ring active:text-teal-500"
+          onClick={redirectBack}
+        >
+          back
+        </button>
+      </div>
     </section>
   );
 }
