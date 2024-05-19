336c2429d0c197e977cff45168ffb3d8dcef2b08
LeeByeongMuk <mansogod@gmail.com>
2024-05-19 22:58:58 +0900

    test: add posts api fail test
    #32

--- src/app/post/page.test.tsx
+++ src/app/post/page.test.tsx
@@ -9,9 +9,19 @@ import React from 'react';
 import { fetchPosts } from '@/app/api/post';
 import { PostsReq } from '@/app/types/post';
 
-const queryClient = new QueryClient();
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
 const wrapper = ({ children }: { children: React.ReactNode }) => (
-  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  <QueryClientProvider client={createTestQueryClient()}>
+    {children}
+  </QueryClientProvider>
 );
 
 // Mock API 호출을 담당하는 함수
@@ -19,44 +29,72 @@ jest.mock('@/app/api/post', () => ({
   fetchPosts: jest.fn(),
 }));
 
-test('useQuery 훅이 정상적으로 데이터를 가져오는지 확인', async () => {
+describe('게시글 목록 페이지 테스트', () => {
   const postsReq: PostsReq = {
     page: 1,
     type: 'notice',
     q: '',
   };
-  // API 호출이 성공적으로 완료될 경우 반환할 데이터
-  const postsData = {
-    data: [
-      {
-        create_at: '2024-01-01 00:00:00',
-        hit: 0,
-        id: 1,
-        title: 'title',
-        type: 'notice',
-        user: { id: 1, name: 'name' },
-      },
-    ],
-  };
 
-  // fetchPosts 함수가 성공적으로 호출될 때 반환할 값을 설정
-  (fetchPosts as jest.Mock).mockResolvedValueOnce(postsData);
+  afterEach(() => {
+    jest.clearAllMocks(); // 각 테스트 후 mock을 초기화합니다.
+  });
 
-  // useQuery 훅을 렌더링합니다.
-  const { result } = renderHook(
-    () =>
-      useQuery({
-        queryKey: ['posts', postsReq],
-        queryFn: () => fetchPosts(postsReq),
-      }),
-    { wrapper }
-  );
+  test('useQuery 훅이 정상적으로 fetchPosts 데이터를 가져오는지 확인', async () => {
+    // API 호출이 성공적으로 완료될 경우 반환할 데이터
+    const postsData = {
+      data: [
+        {
+          create_at: '2024-01-01 00:00:00',
+          hit: 0,
+          id: 1,
+          title: 'title',
+          type: 'notice',
+          user: { id: 1, name: 'name' },
+        },
+      ],
+    };
 
-  // useQuery 훅이 데이터를 가져오기를 기다립니다.
-  await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    // fetchPosts 함수가 성공적으로 호출될 때 반환할 값을 설정
+    (fetchPosts as jest.Mock).mockResolvedValueOnce(postsData);
 
-  // useQuery 훅의 반환값을 확인합니다.
-  expect(result.current.data).toEqual(postsData);
-  expect(result.current.isLoading).toBe(false);
-  expect(result.current.error).toBe(null);
+    // useQuery 훅을 렌더링합니다.
+    const { result } = renderHook(
+      () =>
+        useQuery({
+          queryKey: ['posts', postsReq],
+          queryFn: () => fetchPosts(postsReq),
+        }),
+      { wrapper }
+    );
+
+    // useQuery 훅이 데이터를 가져오기를 기다립니다.
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    // useQuery 훅의 반환값을 확인합니다.
+    expect(result.current.data).toEqual(postsData);
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.error).toBe(null);
+  });
+
+  test('useQuery 훅이 fetchPosts 호출 실패 시 오류를 처리하는지 확인', async () => {
+    const error = new Error('Failed to fetch posts');
+
+    (fetchPosts as jest.Mock).mockRejectedValueOnce(error);
+
+    const { result } = renderHook(
+      () =>
+        useQuery({
+          queryKey: ['posts', postsReq],
+          queryFn: () => fetchPosts(postsReq),
+        }),
+      { wrapper }
+    );
+
+    await waitFor(() => expect(result.current.isError).toBe(true));
+
+    expect(result.current.data).toBeUndefined();
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.error).toEqual(error);
+  });
 });
