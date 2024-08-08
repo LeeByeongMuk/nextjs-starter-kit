22b247e0ea7dcee6fd9e817fa77445e9321ed4ee
LeeByeongMuk <mansogod@gmail.com>
2024-08-08 22:52:08 +0900

    test: update signup testcase
    #32

--- src/app/(auth)/signup/signup.spec.tsx
+++ src/app/(auth)/signup/signup.spec.tsx
@@ -1,37 +1,74 @@
 import '@testing-library/jest-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from '@testing-library/react';
+import { cookies } from 'next/headers';
+import React from 'react';
 
 import Signup from '@/app/(auth)/signup/page';
+import useSignup from '@/app/hooks/auth/useSignup';
+import { server } from '@/mocks/node';
+
+jest.mock('next/headers', () => ({
+  cookies: jest.fn(),
+}));
 
 global.alert = jest.fn();
 
 const queryClient = new QueryClient({
-  defaultOptions: {},
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
 });
 
-describe('회원가입 테스트', () => {
-  beforeEach(() => {
-    // given - 회원가입 페이지가 그려짐
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Signup />
-      </QueryClientProvider>
-    );
+describe('회원가입 테스트', () => {
+  beforeAll(() => {
+    server.listen();
+  });
+
+  beforeEach(() => {
+    server.resetHandlers();
+
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    (cookies as jest.Mock).mockReturnValue({
+      has: jest.fn().mockReturnValue(true),
+      get: jest.fn().mockReturnValue,
+    });
+
+
+    // given - 회원가입 페이지가 그려짐
+    render(<Signup />, { wrapper });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterAll(() => {
+    server.close();
   });
 
   test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다', async () => {
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
     fireEvent.change(screen.getByLabelText('Password'), {
-      target: {
-        value: 'password',
-      },
+      target: { value: 'Password11!!' },
     });
     fireEvent.change(screen.getByLabelText('Password Confirmation'), {
-      target: {
-        value: 'wrongPassword',
-      },
+      target: { value: 'wrongPassword' },
     });
     fireEvent.submit(screen.getByTestId('submit-button'));
 
@@ -40,32 +77,22 @@ describe('회원가입 테스트', () => {
     expect(errorMessage).toBeInTheDocument();
   });
 
-  test('회원가입 인풋 입력란의 필수값을 입력하고 조건의 만족하면 에러 메세지가 표시되지 않는다.', async () => {
+  test('회원가입 인풋 입력란의 필수값을 입력하고 조건의 만족하면 에러 메세지가 표시되지 않는다', async () => {
     // when 회원가입 필수 입력란 입력
     fireEvent.change(screen.getByLabelText('Email address'), {
-      target: {
-        value: 'signin@email.com',
-      },
+      target: { value: 'signin@email.com' },
     });
     fireEvent.change(screen.getByLabelText('Name'), {
-      target: {
-        value: 'name',
-      },
+      target: { value: 'name' },
     });
     fireEvent.change(screen.getByLabelText('NickName'), {
-      target: {
-        value: 'nickname',
-      },
+      target: { value: 'nickname' },
     });
     fireEvent.change(screen.getByLabelText('Password'), {
-      target: {
-        value: 'password',
-      },
+      target: { value: 'Password11!!' },
     });
     fireEvent.change(screen.getByLabelText('Password Confirmation'), {
-      target: {
-        value: 'password',
-      },
+      target: { value: 'Password11!!' },
     });
     fireEvent.submit(screen.getByTestId('submit-button'));
 
@@ -74,5 +101,40 @@ describe('회원가입 테스트', () => {
       const errorMessage = screen.queryByTestId('error-message');
       expect(errorMessage).not.toBeInTheDocument();
     });
+  });
+
+  test('회원가입에 실패하면 에러 알럿이 출력된다', async () => {
+    // when - 회원가입에 실패
+    fireEvent.change(screen.getByLabelText('Email address'), {
+      target: { value: 'test@email.com' },
+    });
+    fireEvent.change(screen.getByLabelText('Name'), {
+      target: { value: 'name' },
+    });
+    fireEvent.change(screen.getByLabelText('NickName'), {
+      target: { value: 'nickname' },
+    });
+    fireEvent.change(screen.getByLabelText('Password'), {
+      target: { value: 'Password11!!' },
+    });
+    fireEvent.change(screen.getByLabelText('Password Confirmation'), {
+      target: { value: 'Password11!!' },
+    });
+    fireEvent.submit(screen.getByTestId('submit-button'));
+
+    const { result } = renderHook(() => useSignup(), { wrapper });
+
+    // then - 에러 알럿이 나타남
+    act(() => {
+      result.current.mutate({
+        email: 'test@email.com',
+        name: 'name',
+        nickname: 'nickname',
+        password: 'wrongPassword',
+      });
+    });
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(window.alert).toHaveBeenCalledWith('Failed to sign up');
+    expect(result.current.data).toBeUndefined();
   });
 });
