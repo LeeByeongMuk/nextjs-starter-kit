4859c3acbeefed2c6fbde54b94ff96c0d1e173a3
LeeByeongMuk <mansogod@gmail.com>
2024-08-10 16:58:04 +0900

    test: add signin page testcase
    #32

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,14 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.post(`/signup`, () => {
+  http.post(`/api/users/signup`, () => {
     return HttpResponse.json({
       data: {
         access_token: '1|mockaccesstoken0000',
       },
     });
   }),
-  http.post(`/api/users/signup`, () => {
+  http.post(`/api/users/signin`, () => {
     return HttpResponse.json({
       data: {
         access_token: '1|mockaccesstoken0000',
