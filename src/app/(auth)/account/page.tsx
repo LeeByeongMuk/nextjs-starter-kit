de0f9dc6d17880823319ac3a362779bd40d5ea74
LeeByeongMuk <mansogod@gmail.com>
2024-08-14 23:17:07 +0900

    refactor: account page code improve
    #32

--- src/app/(auth)/account/page.tsx
+++ src/app/(auth)/account/page.tsx
@@ -1,17 +1,16 @@
 'use client';
 
-import { useMutation } from '@tanstack/react-query';
-import { signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
-import { fetchUserDelete } from '@/app/api/auth';
 import ButtonBox from '@/app/components/Auth/Form/ButtonBox';
 import EmailInput from '@/app/components/Auth/Form/EmailInput';
 import NameInput from '@/app/components/Auth/Form/NameInput';
 import NickNameInput from '@/app/components/Auth/Form/NickNameInput';
 import LayerSpinner from '@/app/components/Spinner/LayerSpinner';
-import useUserUpdate from '@/app/hooks/auth/useUserUpdate';
+import useDeleteAccount from '@/app/hooks/auth/useDeleteAccount';
+import useUpdateAccount from '@/app/hooks/auth/useUpdateAccount';
 
 interface UserUpdateInput {
   email: string;
@@ -20,65 +19,39 @@ interface UserUpdateInput {
 }
 
 export default function Account() {
-  const { data: session, update } = useSession();
+  const { data: session } = useSession();
 
   const methods = useForm<UserUpdateInput>();
   const { handleSubmit } = methods;
 
-  const updateMutate = useUserUpdate();
+  const updateMutate = useUpdateAccount();
 
-  const deleteMutate = useMutation({
-    mutationFn: fetchUserDelete,
-  });
+  const deleteMutate = useDeleteAccount();
 
-  const onSubmit: SubmitHandler<UserUpdateInput> = async data => {
-    updateMutate.mutate(
-      {
-        email: data.email,
-        name: data.name,
-        nickname: data.nickname,
-      },
-      {
-        onSuccess: async () => {
-          alert('Account updated successfully');
-          await update();
-        },
-        onError: () => {
-          alert('Failed to update account');
-        },
-      }
-    );
+  const onUpdateAccount: SubmitHandler<UserUpdateInput> = async data => {
+    updateMutate.mutate({
+      email: data.email,
+      name: data.name,
+      nickname: data.nickname,
+    });
   };
 
-  const onUserDelete = () => {
+  const onDeleteAccount = () => {
     const deletedReason = prompt(
       'Are you sure you want to delete your account? Please enter a reason'
     );
     if (!deletedReason) return;
 
-    deleteMutate.mutate(
-      {
-        deleted_reason: deletedReason,
-      },
-      {
-        onSuccess: async () => {
-          alert('Account deleted successfully');
-          await signOut({
-            callbackUrl: '/',
-          });
-        },
-        onError: () => {
-          alert('Failed to delete account');
-        },
-      }
-    );
+    deleteMutate.mutate({
+      deleted_reason: deletedReason,
+    });
   };
 
   return (
     <FormProvider {...methods}>
       <div className="flex min-h-full flex-col justify-center px-6 py-10 lg:px-8">
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <form className="space-y-4" onSubmit={handleSubmit(onUpdateAccount)}>
             <EmailInput defaultValue={session?.user?.email} />
 
             <NameInput defaultValue={session?.user?.name} />
@@ -94,9 +67,9 @@ export default function Account() {
             <button
               type="button"
               className="flex w-full justify-center rounded-md border border-teal-600 bg-teal-600 px-3 py-1.5 text-sm font-medium text-white hover:bg-transparent hover:text-teal-600 focus:outline-none focus:ring active:text-teal-500"
-              onClick={onUserDelete}
+              onClick={onDeleteAccount}
             >
-              Account Delete
+              Delete Account
             </button>
           </div>
         </div>
