9a707de395df50ac47d615099eff5c582ed084b6
LeeByeongMuk <mansogod@gmail.com>
2024-08-19 00:25:10 +0900

    style: code lint
    #32

--- src/app/components/Post/Form/TitleInput.tsx
+++ src/app/components/Post/Form/TitleInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
+
 import ValidationError from '@/app/components/Error/ValidationError';
 
 interface Props {
--- src/app/hooks/post/useCreatePost.ts
+++ src/app/hooks/post/useCreatePost.ts
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
