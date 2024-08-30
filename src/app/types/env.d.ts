c1318ef575f5079d13659be6e5047d9bb44b3f1d
LeeByeongMuk <mansogod@gmail.com>
2024-08-30 17:03:10 +0900

    feature: add server config
    #75

--- src/app/types/env.d.ts
+++ src/app/types/env.d.ts
@@ -4,6 +4,11 @@ export declare global {
       APP_API_URL: string;
       NEXTAUTH_URL: string;
       NEXTAUTH_SECRET: string;
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_DATABASE: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
     }
   }
 }
