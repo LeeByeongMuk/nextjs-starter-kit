63569e435ca54d2e53e9dd45d0e524bab0afe734
LeeByeongMuk <mansogod@gmail.com>
2024-09-07 00:21:01 +0900

    feature: add error handling utils
    #75

---
+++ src/server/utils/errorHandling.ts
@@ -0,0 +1,30 @@
+import { Response } from 'express';
+
+export class CustomError extends Error {
+  public statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+}
+
+export function handleError(err: unknown, res: Response) {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({
+      ok: false,
+      message: err.message,
+    });
+  } else if (err instanceof Error) {
+    return res.status(500).json({
+      ok: false,
+      message: err.message || 'Internal Server Error',
+    });
+  } else {
+    return res.status(500).json({
+      ok: false,
+      message: 'An unknown error occurred',
+    });
+  }
+}
