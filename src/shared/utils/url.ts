adc1823e3c9f934227423aec8e6b4b08ecf94279
LeeByeongMuk <mansogod@gmail.com>
2025-01-24 15:39:39 +0900

    Update url.ts

--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,5 +1,5 @@
 export const getHandlerURI = (path: string): string => {
-  console.log('process.env', process.env.NODE_ENV);
+  return path;
   if (process.env.NODE_ENV === 'test') {
     const domain = process.env.APP_API_URL || 'http://localhost:9090';
     return `${domain}${path}`;
